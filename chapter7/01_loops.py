3315ddf07442f2af14a52f74a8afd49e46e0295d
TechNinja15 <avneeshjha1506@gmail.com>
2026-01-10 11:49:37 +0000

    fundamental and basics loops

---
+++ chapter7/01_loops.py
@@ -0,0 +1,17 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# the same task can be done in a simple way using loops
+for i in range(1,6):
+    print(i)
+
+# Loops make it easy for a programmer to tell the computer which set of instructions to
+# repeat and how!
+
+# TYPES OF LOOPS IN PYTHON
+# Primarily there are two types of loops in python.
+# • while loops
+# • for loops
