c4855bfbf0f1a2378ecfd1a762635d482d882f56
TechNinja15 <avneeshjha1506@gmail.com>
2026-01-12 13:19:15 +0000

    iterating with for loop

---
+++ chapter7/05_for_loop_iterate.py
@@ -0,0 +1,15 @@
+# For loop with List
+l= [1,4,6,34,56,234]
+
+for i in l:
+    print(i)
+
+# For loop with tuple
+t=(7,43,51,564,87)
+for i in t:
+    print(i)
+
+# For loop with String
+s="AKJ"
+for i in s:
+    print(i)
