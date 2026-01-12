da2b5a98dccc3b4ea3464a9d646cca8f9c83ec15
TechNinja15 <avneeshjha1506@gmail.com>
2026-01-12 13:40:09 +0000

    break and continue statements

---
+++ chapter7/07_break_and_continue.py
@@ -0,0 +1,11 @@
+# Break
+for i in range(100):
+    if(i == 34):
+        break # Exits the loop on the spot
+    print(i)
+
+# Continue
+for i in range(100):
+    if(i == 34):
+        continue # skips the loop on the spot
+    print(i)
