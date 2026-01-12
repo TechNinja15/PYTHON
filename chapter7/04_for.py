ba0fd1979ac8499f80367904e3f949a957ef5273
TechNinja15 <avneeshjha1506@gmail.com>
2026-01-12 12:59:49 +0000

    basics of for loop

---
+++ chapter7/04_for.py
@@ -0,0 +1,8 @@
+# printing 0-4 using for loop
+for i in range(0,4):
+    print(i)
+
+# The range() function in python is used to generate a sequence of number.
+
+# SYNTAX: 
+#range(start, stop, step_size) 
